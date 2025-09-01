8fec4696fe8c1e5f27d01d84acbb52ac807e306e
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-01 22:49:02 +0300

    Add ReadApp project to read two integers and display their sum

---
+++ ReadApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ReadApp
+    
+    ///<summary>
+    ///Reads two integers and print the result
+    /// </summary>
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int result = 0;
+
+            Console.WriteLine("Please insert two integers");
+            num1 = int.Parse(Console.ReadLine()!);
+            num2 = int.Parse(Console.ReadLine()!);
+
+            result = num1 + num2;
+
+            Console.WriteLine($"{num1} + {num2} = {result}");
+
+        }
+    }
+}
