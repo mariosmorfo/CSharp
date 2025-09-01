32ec8b4206b728a282cc53e2aac380d4ef7c1006
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-01 22:45:10 +0300

    Add FactorialApp with iterative and recursive factorial implementations

---
+++ FactorialApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+
+namespace FactorialApp
+    ///<summary>
+    ///Iterative versiob of factorial
+    ///</summary>
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++) 
+            { 
+                Console.WriteLine($"{i} ! = {FactoIterative(i):N0}");
+            }
+
+        }
+
+        public static BigInteger FactoIterative(int n)
+        {
+            BigInteger result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+
+            }
+            return result;
+        }
+
+        public static BigInteger FactoRecursive(int n)
+        {
+            return (n > 1) ? n * FactoRecursive(n - 1) : 1;
+            
+        }
+    }
+}
