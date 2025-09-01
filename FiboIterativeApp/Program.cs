48112c38dbfb12c19b5f84fdc8b10956f9d34711
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-01 22:45:47 +0300

    Add FiboIterativeApp with iterative Fibonacci function

---
+++ FiboIterativeApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FiboIterativeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static int Fibo(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 2;
+
+            for (int i = 1; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return (n != 0) ? c : 0;
+        }
+    }
+}
