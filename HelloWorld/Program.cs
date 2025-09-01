ac88f909b2584e70c51f0d81db4bd99f218df887
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-01 22:41:58 +0300

    Initial commit of HelloWorld project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    /// <summary>
+    /// First Programm Hello World
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Coding Factory");
+            Console.WriteLine();
+        }
+    }
+}
