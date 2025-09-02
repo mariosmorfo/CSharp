d2b98855730beb4a73d3f293eecc9c3c825b77df
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-02 19:55:19 +0300

    Implement power calculation using BigInteger

---
+++ PowerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace PowerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int myBase = 2;
+           int power = 10;
+           BigInteger result = 1;
+
+            for (int i = 1; i >= power; i++)
+            {
+                result *= myBase;
+            }
+            Console.WriteLine($"{myBase} ^ {power} = {result}");
+        }
+    }
+}
