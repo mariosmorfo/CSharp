091531f403405ac83a3f9e9c3375d5693c9c3699
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-02 19:56:24 +0300

    Add simple regex match example

---
+++ RegEx/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = "hello123";
+            string pattern1 = @"\d+";
+
+            OneMatch(input1, pattern1);
+         
+        }
+
+        public static void OneMatch(string input, string pattern)
+        {
+            Match match = Regex.Match(input, pattern);
+
+            if (match.Success)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
