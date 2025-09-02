33e3404a5382e807b617876499c80b477a96a1a1
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-02 19:55:50 +0300

    Add prime number checker with loop optimization

---
+++ PrimesApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                Console.WriteLine($"{i} : is prime : {isPrime(i)}");
+            }
+        }
+
+        public static bool isPrime(int n)
+        {
+            bool prime = true;
+
+            for (int divider = 2; divider <= (int)Math.Sqrt(n); divider++)
+            {
+                if ((n % divider) == 0)
+                {
+                    prime = false;
+                    break;
+                } 
+
+            }
+            return prime;
+        }
+
+    }
+}
