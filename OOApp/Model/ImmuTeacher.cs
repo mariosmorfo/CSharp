13777ea75be808c8193c84b12ac0de0c29ffa182
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-03 14:40:01 +0300

    first Object oriented program with teacher

---
+++ OOApp/Model/ImmuTeacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOApp.Model
+{
+    internal sealed class ImmuTeacher
+    {
+        public int Id { get; init; }
+        public string? FirstName { get; init; }
+        public string? Lastname { get; init; }
+
+        public ImmuTeacher() { }
+
+        public ImmuTeacher(int id, string? firstName, string? lastname)
+        {
+            Id = id;
+            FirstName = firstName;
+            Lastname = lastname;
+        }
+    }
+}
---
+++ OOApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using OOApp.Model;
+
+namespace OOApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Teacher alice = new Teacher();
+           Teacher bob = new();
+           var kostas = new Teacher();
+
+            Teacher anna = new Teacher() //Object Initializer
+            {
+                Id = 1,
+                Lastname = "Papa",
+                Firstname = "Anna"
+            };
+
+            Teacher andreas = new Teacher(1, "Andreas", "Mosxos");
+        }
+    }
+}
