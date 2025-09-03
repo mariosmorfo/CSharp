42e8036fc10c5608c5279fe98f2403148a154046
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-03 14:34:53 +0300

    Add example of optional parameters in GetDateTime method

---
+++ OptionalParams/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OptionalParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = GetDateTime();
+            Console.WriteLine($"{dateTime:F}");
+        }
+
+        public static DateTime GetDateTime(int year = 2025, int month = 9, int day = 3, int min = 0, int sec = 0, int milli = 0)
+        { 
+            return new DateTime(year, month, day, min, sec, milli, DateTimeKind.Utc); 
+        }
+    }
+}
