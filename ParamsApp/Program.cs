841fd83e1a6b142a320f2e874fff803b5ae82dfe
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-03 14:35:23 +0300

    Add example of using params keyword with Add method

---
+++ ParamsApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ParamsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            result = Add(1, 5, 6, 8);
+            Console.WriteLine(result);
+
+            result = Add(1, 4, 5, 7, 9);
+            Console.WriteLine(result);
+        }
+
+        public static int Add(params int[] nums)
+        {
+            int sum = 0;
+            foreach (int el in nums)
+            { 
+                sum += el;
+            }
+            return sum;
+        }
+    }
+}
