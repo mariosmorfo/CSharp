ef77918002231f5883983ef14a1dbced6322f123
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-04 21:10:05 +0300

    Implement Student class with constructors, reference return, shallow copy, and deep copy methods

---
+++ CopyingApp/Model/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyingApp.Model
+{
+    internal class Address
+    {
+        public string? StreetName { get; set; }
+        public string? StreetNo { get; set; }
+        public string? ZipCode { get; set; }
+
+        public Address() { }
+
+        public Address(string? streetName, string? streetNo, string? zipCode)
+        {
+            StreetName = streetName;
+            StreetNo = streetNo;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{StreetName} {StreetNo} {ZipCode}";
+        }
+    }
+}
---
+++ CopyingApp/Model/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyingApp.Model
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public Address? Address { get; set; }
+
+        public Student() { }
+
+        public Student(int id, string? firstName, string? lastName, Address? address)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+        }
+
+        public Student GetRef()
+        {
+            return this;
+        }
+
+        public Student GetShallowCopy()
+        {
+            return (Student)MemberwiseClone();
+        }
+
+        public Student GetDeepCopy()
+        {
+            Student student = (Student)MemberwiseClone();
+            student.Address = new Address()
+            {
+                StreetName = Address!.StreetName,
+                StreetNo = Address!.StreetNo,
+                ZipCode = Address!.ZipCode
+            };
+            return student;
+        }
+    }
+}
