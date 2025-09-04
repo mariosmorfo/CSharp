9384da7407feba7d37de080d369a5f7f7df964ea
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-04 21:08:59 +0300

    Implement Cat class inheriting from AbstractAnimal with Speak, Eat, and ToString overrides

---
+++ AbstractClassesApp/Model/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesApp.Model
+{
+    internal class Cat : AbstractAnimal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Cat says hello");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Cat is eating");
+        }
+        public override string ToString()
+        {
+           return "Id" + Id + " , Name: " + Name + "Age: " + Age;
+        }
+    }
+    
+        
+ }
+
