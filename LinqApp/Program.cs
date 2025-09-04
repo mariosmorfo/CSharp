649302ea820fa954cc20e075bc81eb59baae376a
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-04 21:12:17 +0300

    Add LINQ examples with query syntax and fluent API for filtering, mapping, and aggregation

---
+++ LinqApp/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace LinqApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+            /*
+             * Query Syntax
+             */
+
+            IEnumerable<int> allNumbers = from num in numbers select num;
+            foreach ( var num in numbers )
+            {
+                Console.WriteLine( num );
+            }
+
+            var oddNums = from num in numbers where num % 2 !=0 select num;
+            foreach( var num in oddNums )
+            {
+                Console.WriteLine( num );
+            }
+
+            var evenNums = from num in numbers where num % 2 == 0 select num;
+            foreach(var num in evenNums)
+            {
+                Console.WriteLine( num );
+            }
+            evenNums.ToList().ForEach(x => Console.WriteLine( x ));
+
+            List<int> evensOrderedDesc = (from num in numbers 
+                                          where num % 2 == 0
+                                          orderby num descending
+                                          select num).ToList();
+
+            evensOrderedDesc.ForEach(x => Console.WriteLine( x ));
+        
+
+        /*
+         * Fluent API
+         */
+
+            //Filtering
+            var evenNumsFluent = numbers.Where(n=> n % 2 == 0);
+            Console.WriteLine(string.Join(", ", evenNumsFluent));
+
+            //Mapping
+            var squares = numbers.Select(n => n * n).ToList();
+            squares.ForEach(Console.WriteLine);
+
+            //Reduce
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
+
+            //Min, Max, Average
+            var min = numbers.Min();
+            var max = numbers.Max();
+            var avg = numbers.Average();
+            Console.WriteLine(min + ", " + max + " ," + avg);
+
+            var anyGE10 = numbers.Any(n => n >= 10);
+            Console.WriteLine("Any GE 10: " + anyGE10);
+
+            var allGT0 = numbers.All(n => n> 0);
+            Console.WriteLine("All GE 10: "+ allGT0);
+
+            var odds = numbers.Where(n => n % 2 != 0).Select(n => n * 2).ToList();
+
+        }
+    }
+}
