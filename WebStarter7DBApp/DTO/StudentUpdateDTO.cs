51c89d0c67afe61c3af32238ea9cf3a1c71c1d0f
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-06 22:17:12 +0300

    add StudentUpdateDTO : BaseDTO with validation

---
+++ WebStarter7DBApp/DTO/StudentUpdateDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStarter7DBApp.DTO
+{
+    public class StudentUpdateDTO : BaseDTO
+    {
+        [Required(ErrorMessage = "Firstname is required")]
+        [MinLength(1, ErrorMessage = "Firstname must be at least 1 character long")]
+
+        public string? Firstname { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        [MinLength(1, ErrorMessage = "Lastname must be at least 1 character long")]
+        public string? Lastname { get; set; }
+
+        public StudentUpdateDTO() { }
+
+        public StudentUpdateDTO(int id,string? firstname, string? lastname)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            
+        }
+    }
+}
