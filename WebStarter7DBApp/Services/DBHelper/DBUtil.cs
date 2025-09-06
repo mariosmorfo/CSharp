464d605ccd952aaf7ef1b93576086ede5eb8d868
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-06 22:18:36 +0300

    add DBUtil to create SqlConnection from config

---
+++ WebStarter7DBApp/Services/DBHelper/DBUtil.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WebStarter7DBApp.Services.DBHelper
+{
+    public static class DBUtil
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection;
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsetting.json");
+            var configuaration = builder.Build();
+            string? url = configuaration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                connection = new SqlConnection(url);
+                return connection;
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
