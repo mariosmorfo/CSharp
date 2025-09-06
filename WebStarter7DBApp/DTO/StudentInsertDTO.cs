38c2a40dbdfa020bcd48298f9a353def637a4faf
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-06 22:16:30 +0300

    add StudentInsertDTO with DataAnnotations validation

---
+++ WebStarter7DBApp/DTO/StudentInsertDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStarter7DBApp.DTO
+{
+    public class StudentInsertDTO
+    {
+        [Required(ErrorMessage = "Firstname is required")]
+        [MinLength(1, ErrorMessage = "Firstname must be at least 1 character long")]
+
+        public String? Firstname { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        [MinLength(1, ErrorMessage = "Lastname must be at least 1 character long")]
+        public String? Lastname { get; set; }
+
+        public StudentInsertDTO() { }
+
+        public StudentInsertDTO(string? firstname, string? lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
