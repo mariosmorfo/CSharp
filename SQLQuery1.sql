ff464d0d49097d664d4c642f6d438165eb7c794f
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-06 22:19:46 +0300

    sql Student insert query

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿USE Students7DB;
+GO
+
+CREATE TABLE Students (
+	id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Firstname NVARCHAR(50) NOT NULL,
+	Lastname NVARCHAR(50) NOT NULL,
+)
+
+GO
+
+INSERT INTO Students (Firstname, Lastname)
+VALUES 
+	('Μάριος', 'Μορφονίδης'),
+	('Αλέξης', 'Παπάς'),
+	('Νίκος', 'Ιωάννου');
