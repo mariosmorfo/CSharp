5f48aef0d9494b113a018748ce0b33e20857201c
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-07 21:12:12 +0300

    Configure Program.cs with MVC, SQL Server DbContext, and default routing

---
+++ SchoolApp/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.Data;
+
+namespace SchoolApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //AddDbContext is scoped - per request a new instance of the dbcontext is created
+            builder.Services.AddDbContext<Mvc7DbContext>(options => options.UseSqlServer(connString));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
