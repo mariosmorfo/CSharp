19bc72e1f6e8b5e1a6d6531b8eeb2e7e1255f331
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-07 21:10:26 +0300

    Add User entity with UserRole enum and relations to Student and Teacher

---
+++ SchoolApp/Data/User.cs
@@ -0,0 +1,26 @@
+﻿using SchoolApp.Core.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace SchoolApp.Data;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public UserRole? UserRole { get; set; } = null!;
+
+    public virtual Student? Student { get; set; }
+
+    public virtual Teacher? Teacher { get; set; }
+}
