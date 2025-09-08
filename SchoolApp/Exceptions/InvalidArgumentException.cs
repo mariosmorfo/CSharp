30bbca7f3acf915726ec6e087ef830d3a4948d0f
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-08 20:57:45 +0300

    add InvalidArgumentException for invalid parameter handling

---
+++ SchoolApp/Exceptions/InvalidArgumentException.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.Exceptions
+{
+    public class InvalidArgumentException : AppException
+    {
+        private static readonly string DEFAULT_CODE = "Invalid Argument";
+
+        public InvalidArgumentException(string code, string message) : base(code + DEFAULT_CODE, message)
+        {
+
+        }
+    }
+}
