89653ba6e179d9df6013fa5821dc398d8d4411d3
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-08 20:56:41 +0300

    add EntityAlreadyExistsException for handling duplicate entity errors

---
+++ SchoolApp/Exceptions/EntityAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.Exceptions
+{
+    public class EntityAlreadyExistsException : AppException
+    {
+        private static readonly string DEFAULT_CODE = "Already Exists";
+
+        public EntityAlreadyExistsException(string code, string message) : base(code + DEFAULT_CODE, message) 
+        {
+
+        }
+    }
+}
