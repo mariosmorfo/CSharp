e8ce5fb735aedef2db720156bdefd8af9639caa9
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-09-08 20:54:49 +0300

    refactor username

--- SchoolApp/Data/User.cs
+++ SchoolApp/Data/User.cs
@@ -8,7 +8,7 @@ public partial class User
 {
     public int Id { get; set; }
 
-    public string UserName { get; set; } = null!;
+    public string Username { get; set; } = null!;
 
     public string Email { get; set; } = null!;
 
